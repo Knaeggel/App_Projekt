85265f7c887beeb40d134f7090851b4aba37906c
nschwen1 <nschwen1@stud.hs-offenburg.de>
2022-11-21 09:45:37 +0100

    added potential table for recepie

--- app/src/main/java/com/jn/rezepte_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/jn/rezepte_app/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,8 +25,13 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
+        final TextView textView = binding.textRecepie;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+
+        TableLayout table = binding.recepieTable;
+
 
 
 
